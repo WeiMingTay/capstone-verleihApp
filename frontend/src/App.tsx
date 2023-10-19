905446b5fed27d0dacc2cf1b1437e784eeda7fc3
weimingtay <ming23.tay@gmail.com>
2023-10-19 23:19:50 +0200

    flip Card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './assets/App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AddTool from "./pages/AddTool/AddTool.tsx";
+import FlipCard from "./pages/FlipCard/FlipCard.tsx";
 
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
             <h1>Moin</h1>
             <Routes>
                 <Route path={"werkzeuge/add"} element={<AddTool/>}/>
+                <Route path={"ulrike/flipCard"} element={<FlipCard/>}/>
             </Routes>
         </BrowserRouter>
     )
---
+++ frontend/src/pages/FlipCard/FlipCard.tsx
@@ -0,0 +1,15 @@
+import "./FlipCard.scss"
+
+export default function FlipCard() {
+    return (
+        <section className={"container"}>
+            <div className="flipCard-container">
+                <div className="card">
+                    <div className="front"></div>
+                    <div className="back">
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
