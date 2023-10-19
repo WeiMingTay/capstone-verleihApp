c79be8d376b753b67d2518d254218b5fad71e4ed
weimingtay <ming23.tay@gmail.com>
2023-10-19 23:33:50 +0200

    link zur Karte

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import './assets/App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import AddTool from "./pages/AddTool/AddTool.tsx";
 import FlipCard from "./pages/FlipCard/FlipCard.tsx";
 
@@ -10,6 +10,7 @@ export default function App() {
     return (
         <BrowserRouter>
             <h1>Moin</h1>
+            <Link to={"ulrike/flipCard"}>Ulrike</Link>
             <Routes>
                 <Route path={"werkzeuge/add"} element={<AddTool/>}/>
                 <Route path={"ulrike/flipCard"} element={<FlipCard/>}/>
