192a008227f7175d52d6d5dd1e9e115f5af0c3a7
weimingtay <ming23.tay@gmail.com>
2023-10-19 21:48:33 +0200

    Anzeige und form funktioniert. Input/select springt wieder auf Ausgangswert zurück, nach Absenden des Inhalts

--- frontend/src/pages/AddTool/AddTool.tsx
+++ frontend/src/pages/AddTool/AddTool.tsx
@@ -1,36 +1,80 @@
 import axios from "axios";
-import {allCategories} from "../../assets/entities/tools.ts";
+import {allCategories, Tools} from "../../assets/entities/tools.ts";
 import "./AddTool.scss"
+import {ChangeEvent, FormEvent, useState} from "react";
 
 
 export default function AddTool() {
+    const [, setTool] = useState<Tools>()
+    const [name, setName] = useState("")
+    const [location, setLocation] = useState("")
+    const [category, setCategory] = useState("")
 
-    function submitNewTool() {
-        axios("api/tools/add")
+
+    function submitNewTool(event: FormEvent) {
+
+        event.preventDefault()
+        axios.post("/api/tools/add",
+            {
+                name: name,
+                location: location,
+                category: category
+            })
             .then((response) => {
-                console.log(response.data);
+                setTool(response.data)
+                resetForm()
             })
             .catch((error) => {
                 console.error(error);
             })
+
+    }
+
+    function resetForm() {
+        setName("");
+        setLocation("");
+        setCategory("");
+
+    }
+
+    function changeName(event: ChangeEvent<HTMLInputElement>) {
+        const newBezeichnung = event.target.value;
+        setName(newBezeichnung);
+
+    }
+
+    function changeLocation(event: ChangeEvent<HTMLInputElement>) {
+        const newLocation = event.target.value;
+        setLocation(newLocation);
+    }
+
+    function changeCategory(event: ChangeEvent<HTMLSelectElement>) {
+        const newCategory = event.target.value;
+        setCategory(newCategory);
     }
 
     return (
         <div className="addToolPage">
             <form onSubmit={submitNewTool}>
-                <label><input type="text" placeholder="Gerätebezeichnung"/></label>
-                <label><input type="text" placeholder="Location"/></label>
-                <label><select>
+                <label><input type="text" placeholder="Gerätebezeichnung" onInput={changeName} value={name}
+                              required/></label>
+                <label><input type="text" placeholder="Location" onInput={changeLocation} value={location}
+                              required/></label>
+                <label><select onChange={changeCategory} value={category} required>
+                    <option value="" disabled >Kategorie</option>
                     {allCategories.map(category => {
                         return <option
                             key={category}
                             value={category}>{category.charAt(0).toUpperCase() + category.slice(1).toLowerCase()}
                         </option>
-
                     })
                     }
                 </select></label>
+                <button>Speichern</button>
             </form>
+            <p>Bezeichnung: {name}</p>
+            <p>Location: {location}</p>
+            <p>Kategorie: {category}</p>
         </div>
     );
 }
