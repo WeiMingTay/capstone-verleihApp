e4b945d3ba806987049bdd9a46b6587658a14e7b
weimingtay <ming23.tay@gmail.com>
2023-10-19 11:33:15 +0200

    Setup Frontend. SCSS hakt und Bedarf einen Neustart - Zwischenstopp
    TODO:
    ein IntegrationTest

---
+++ frontend/src/pages/AddTool/AddTool.tsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import {allCategories} from "../../assets/entities/tools.ts";
+import "./AddTool.css"
+
+
+export default function AddTool() {
+
+    function submitNewTool() {
+        axios("api/tools/add")
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }
+
+    return (
+        <div className="addToolPage">
+            <form onSubmit={submitNewTool}>
+                <label><input type="text" placeholder="Gerätebezeichnung"/></label>
+                <label><input type="text" placeholder="Location"/></label>
+                <label><select>
+                    {allCategories.map(category => {
+                        return <option
+                            key={category}
+                            value={category}>{category.charAt(0).toUpperCase() + category.slice(1).toLowerCase()}
+                        </option>
+
+                    })
+                    }
+                </select></label>
+            </form>
+        </div>
+    );
+}
+
