9efb66e806432eb233586a8351731d056823731c
weimingtay <ming23.tay@gmail.com>
2023-10-20 18:41:55 +0200

    Hamburger Menü gebaut
    TO-DO:
    - Header mit Navigation
    - Informationen was auf den Seiten dargestellt werden soll

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,19 +1,32 @@
 import {Link} from "react-router-dom";
 import "./Header.scss"
+import {useState} from "react";
 
 export default function Header() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
     return (
         <header>
             <div>
                 <img src="../../../public/logo.jpg" alt="Logo"/>
                 <h1>Capstone</h1>
             </div>
-            <nav>
-                <ul>
-                    <li><Link to={"/start"}>Start</Link></li>
-                    <li><Link to={"/werkzeuge"}>Werkzeuge</Link></li>
-                </ul>
-            </nav>
+            <div className={`menu-toggle ${isOpen ? "open" : ""}`}>
+                <div onClick={toggleMenu}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
+                <nav>
+                    <ul>
+                        <li><Link to={"/start"}>Start</Link></li>
+                        <li><Link to={"/werkzeuge"}>Werkzeuge</Link></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
