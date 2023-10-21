af7d1881d1fe57cdef519b6b21fba2bd68df5448
weimingtay <ming23.tay@gmail.com>
2023-10-22 00:56:31 +0200

    ServiceTests geschrieben und getestet
    TODO:
    IntegrationTests
    Frontend

--- backend/src/test/java/de/neuefische/backend/tools/ToolsServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/tools/ToolsServiceTest.java
@@ -2,6 +2,9 @@ package de.neuefische.backend.tools;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -12,6 +15,45 @@ class ToolsServiceTest {
 
     Tool tool1 = new Tool("Hammer", Category.TOOLS, "Keller");
 
+    // GETall
+    @Test
+    void getAllTools_expectOneTool() {
+        // GIVEN
+        List<Tool> toolList = new ArrayList<>();
+        toolList.add(tool1);
+        // toolList.add(new Tool("Bohrmaschine", Category.TOOLS, "Keller"));
+        //WHEN
+        when(toolsRepository.findAll()).thenReturn(toolList);
+        List<Tool> actual = toolsService.getAllTools();
+
+        //THEN
+        List<Tool> expected = List.of(new Tool(
+                "Hammer",
+                Category.TOOLS,
+                "Keller"));
+
+        verify(toolsRepository).findAll();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getAllTools_expectEmptyList() {
+
+        //GIVEN
+        List<Tool> movieList = List.of();
+
+        //WHEN
+        when(toolsRepository.findAll()).thenReturn(movieList);
+        List<Tool> actual = toolsService.getAllTools();
+
+        //THEN
+        List<Tool> expected = List.of();
+
+        verify(toolsRepository).findAll();
+        assertEquals(expected, actual);
+    }
+
+    // POST newTool
     @Test
     void createTool_expectCreatedToolObject() {
         //GIVEN
@@ -44,4 +86,6 @@ class ToolsServiceTest {
         verify(toolsRepository).save(expected);
         assertNotEquals(expected, actual);
     }
+
+
 }
