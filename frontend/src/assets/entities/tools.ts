bb735bceff175641c2ba06f7b8528139bafa6175
weimingtay <ming23.tay@gmail.com>
2023-10-24 18:32:35 +0200

    Deutsche Übersetzung der Kategorien hinzugefügt

--- frontend/src/assets/entities/tools.ts
+++ frontend/src/assets/entities/tools.ts
@@ -12,4 +12,20 @@ export type Tools = {
 
 
 type Category = "GARDEN" | "TOOLS" | "KITCHEN" | "PLUMBING" | "ELECTRONICS" | "SEWING" | "OTHER";
+
+const categoryTranslations: Record<Category, string> = {
+    GARDEN: "GARTEN",
+    TOOLS: "WERKZEUGE",
+    KITCHEN: "KÜCHE",
+    PLUMBING: "SANITÄR",
+    ELECTRONICS: "ELEKTRONIK",
+    SEWING: "NÄHEN & STRICKEN",
+    OTHER: "SONSTIGES",
+};
+
 export const allCategories: Category[] = ["GARDEN", "TOOLS", "KITCHEN", "PLUMBING", "ELECTRONICS", "SEWING", "OTHER"];
+
+// Funktion zum Abrufen der deutschen Übersetzung
+export function getCategoryTranslation(category: Category): string {
+    return categoryTranslations[category];
+}
