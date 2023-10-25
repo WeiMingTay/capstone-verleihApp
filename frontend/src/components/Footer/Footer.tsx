5b5f6f846fc318e10d76f0026eec32998c3d74f0
weimingtay <ming23.tay@gmail.com>
2023-10-25 22:39:56 +0200

    Header Position wieder gefixt nav Anordnung
    TO-DO:

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,9 +1,28 @@
 import "./Footer.scss";
+import {Link, useLocation} from "react-router-dom";
 
 export default function Footer() {
+    const location = useLocation();
+
+
+    const isOnWelcomePage = location.pathname === "/";
+    if (isOnWelcomePage) {
+        return null;
+    }
+
+    /* TODO
+     if (isUserLoggedIn) {
+          return null;
+      }*/
+
     return (
         <footer>
-            <p>Anmelden</p>
+            <p>Noch nicht angemeldet?</p>
+            <div>
+                <Link to={"/"}>Einloggen</Link>
+                <span> oder </span>
+                <Link to={"/"}>Registrieren</Link>
+            </div>
         </footer>
     );
 }
