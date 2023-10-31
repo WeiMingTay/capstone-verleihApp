ebf592bffa0176e39a978d1a59efa16d656cf070
weimingtay <ming23.tay@gmail.com>
2023-10-31 18:41:16 +0100

    bugfix. id ist vom type Category
    TO-DO:

--- frontend/src/pages/CategoryPage/CategoryPage.tsx
+++ frontend/src/pages/CategoryPage/CategoryPage.tsx
@@ -1,28 +1,27 @@
-import {useParams} from "react-router-dom";
-import {getCategoryTranslation, Tools} from "../../assets/entities/tools.ts";
+import { useParams } from "react-router-dom";
+import {Category, getCategoryTranslation, Tools} from "../../assets/entities/tools.ts";
 import ToolCard from "../../components/ToolCard/ToolCard.tsx";
 
-
 type Props = {
-    readonly tools: Tools[]
-}
-export default function CategoryPage(props: Props) {
-    const cat = useParams();
+    readonly tools: Tools[];
+};
 
-    if ((props.tools.filter(tool => tool.categories.includes(cat.id))).length === 0) {
-        return <p>Keine Elemente mit Kategorie {getCategoryTranslation(cat.id)} vorhanden</p>
+export default function CategoryPage(props: Props) {
+    const { id } = useParams();
+    const categoryId = id as Category;
+
+    if (!id || (props.tools.filter(tool => tool.categories.includes(categoryId)).length === 0)) {
+        return <p>Keine Elemente mit Kategorie {getCategoryTranslation(categoryId)} vorhanden</p>;
     }
 
-    return (<>
-        <h2>{cat.id}</h2>
-        <section>
-
-            {
-                props.tools.filter(tool => tool.categories.includes(cat.id)).map(tool => <ToolCard key={tool.id}
-                                                                                                   tool={tool}/>)
-            }
-
-        </section>
-    </>)
-
+    return (
+        <>
+            <h2>{categoryId}</h2>
+            <section>
+                {props.tools
+                    .filter(tool => tool.categories.includes(categoryId))
+                    .map(tool => <ToolCard key={tool.id} tool={tool} />)}
+            </section>
+        </>
+    );
 }
