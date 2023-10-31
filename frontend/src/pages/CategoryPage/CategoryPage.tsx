6ff8108f70a052d9787ca4e601e19619a6289bcb
weimingtay <ming23.tay@gmail.com>
2023-10-31 20:17:22 +0100

    Fall, dass es keine Elemente einer Kategorie gibt, abgedeckt TO-DO:

--- frontend/src/pages/CategoryPage/CategoryPage.tsx
+++ frontend/src/pages/CategoryPage/CategoryPage.tsx
@@ -1,27 +1,30 @@
-import { useParams } from "react-router-dom";
-import {Category, getCategoryTranslation, Tools} from "../../assets/entities/tools.ts";
+import {useParams} from "react-router-dom";
+import {Category, getCategoryImage, getCategoryTranslation, Tools} from "../../assets/entities/tools.ts";
 import ToolCard from "../../components/ToolCard/ToolCard.tsx";
+import "./CategoryPage.scss"
 
 type Props = {
     readonly tools: Tools[];
 };
 
 export default function CategoryPage(props: Props) {
-    const { id } = useParams();
+    const {id} = useParams();
     const categoryId = id as Category;
 
-    if (!id || (props.tools.filter(tool => tool.categories.includes(categoryId)).length === 0)) {
-        return <p>Keine Elemente mit Kategorie {getCategoryTranslation(categoryId)} vorhanden</p>;
-    }
 
     return (
-        <>
-            <h2>{categoryId}</h2>
-            <section>
-                {props.tools
-                    .filter(tool => tool.categories.includes(categoryId))
-                    .map(tool => <ToolCard key={tool.id} tool={tool} />)}
-            </section>
-        </>
-    );
+        <div className={"CategoryPage"}>
+            <h2 style={{backgroundImage: `url(${getCategoryImage(categoryId)})`}}>{getCategoryTranslation(categoryId)}</h2>
+            {
+                (props.tools.filter(tool => tool.categories.includes(categoryId)).length === 0)
+                    ? <p>Keine Elemente mit dieser Kategorie vorhanden</p>
+                    : <section>
+                        {props.tools
+                            .filter(tool => tool.categories.includes(categoryId))
+                            .map(tool => <ToolCard key={tool.id} tool={tool}/>)}
+                    </section>
+            }
+        </div>
+    )
+        ;
 }
