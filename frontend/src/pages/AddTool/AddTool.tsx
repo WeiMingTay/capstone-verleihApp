b52d83cd13624c8c4aca25d96e43d0c74179b5c5
weimingtay <ming23.tay@gmail.com>
2023-11-09 22:36:57 +0700

    Einfachen Ladebildschirm bei AddTool

--- frontend/src/pages/AddTool/AddTool.tsx
+++ frontend/src/pages/AddTool/AddTool.tsx
@@ -26,6 +26,7 @@ export default function AddTool(props: Props) {
     const [author, setAuthor] = useState("")
     const [description, setDescription] = useState("")
 
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
 
@@ -37,6 +38,7 @@ export default function AddTool(props: Props) {
 
     function submitNewTool(event: FormEvent): void {
         event.preventDefault();
+        setLoading(true);
 
         const selectedCategoryValues: string[] = selectedOptions.map(option => option.value);
 
@@ -56,14 +58,14 @@ export default function AddTool(props: Props) {
             description: description,
             timestamp: new Date().toISOString()
         }
-/*[], {
-                day: '2-digit',
-                month: 'long',
-                year: 'numeric',
-                hour: '2-digit',
-                minute: '2-digit',
+        /*[], {
+                        day: '2-digit',
+                        month: 'long',
+                        year: 'numeric',
+                        hour: '2-digit',
+                        minute: '2-digit',
 
-            }*/
+                    }*/
         if (imageFile) {
             formData.append("file", imageFile);
         }
@@ -85,6 +87,9 @@ export default function AddTool(props: Props) {
             })
             .catch(error => {
                 console.error("Fehler beim POST", error);
+            })
+            .finally(() => {
+                setLoading(false);
             });
 
     }
@@ -153,6 +158,36 @@ export default function AddTool(props: Props) {
 
     return (
         <div className="addToolPage">
+            {loading &&
+                <div className="ladebildschirm">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 100 100"
+                        preserveAspectRatio="xMidYMid"
+                        style={{
+                            margin: 'auto',
+                            display: 'block',
+                            shapeRendering: 'auto'
+                        }}
+                        width="200px"
+                        height="200px"
+                    >
+                        <g transform="translate(50 50)">
+                            <g>
+                                <animateTransform attributeName="transform"
+                                                  type="rotate"
+                                                  values="0;45"
+                                                  keyTimes="0;1"
+                                                  dur="1000ms"
+                                                  repeatCount="indefinite">
+                                </animateTransform>
+                                <path
+                                    d="M29.491524206117255 -5.5 L37.491524206117255 -5.5 L37.491524206117255 5.5 L29.491524206117255 5.5 A30 30 0 0 1 24.742744050198738 16.964569457146712 L24.742744050198738 16.964569457146712 L30.399598299691117 22.621423706639092 L22.621423706639096 30.399598299691114 L16.964569457146716 24.742744050198734 A30 30 0 0 1 5.5 29.491524206117255 L5.5 29.491524206117255 L5.5 37.491524206117255 L-5.499999999999997 37.491524206117255 L-5.499999999999997 29.491524206117255 A30 30 0 0 1 -16.964569457146705 24.742744050198738 L-16.964569457146705 24.742744050198738 L-22.621423706639085 30.399598299691117 L-30.399598299691117 22.621423706639092 L-24.742744050198738 16.964569457146712 A30 30 0 0 1 -29.491524206117255 5.500000000000009 L-29.491524206117255 5.500000000000009 L-37.491524206117255 5.50000000000001 L-37.491524206117255 -5.500000000000001 L-29.491524206117255 -5.500000000000002 A30 30 0 0 1 -24.742744050198738 -16.964569457146705 L-24.742744050198738 -16.964569457146705 L-30.399598299691117 -22.621423706639085 L-22.621423706639092 -30.399598299691117 L-16.964569457146712 -24.742744050198738 A30 30 0 0 1 -5.500000000000011 -29.491524206117255 L-5.500000000000011 -29.491524206117255 L-5.500000000000012 -37.491524206117255 L5.499999999999998 -37.491524206117255 L5.5 -29.491524206117255 A30 30 0 0 1 16.964569457146702 -24.74274405019874 L16.964569457146702 -24.74274405019874 L22.62142370663908 -30.39959829969112 L30.399598299691117 -22.6214237066391 L24.742744050198738 -16.964569457146716 A30 30 0 0 1 29.491524206117255 -5.500000000000013 M0 -20A20 20 0 1 0 0 20 A20 20 0 1 0 0 -20"
+                                    ></path>
+                            </g>
+                        </g>
+                    </svg>
+                </div>}
             <form onSubmit={submitNewTool}>
                 <label>
                     <input
