2428cf0a5866ce79d40507c02721e6a902145a16
weimingtay <ming23.tay@gmail.com>
2023-11-10 20:32:07 +0700

    Bei Delete öffnet sich ein DialogFenster

--- frontend/src/pages/Tool/ToolPage.tsx
+++ frontend/src/pages/Tool/ToolPage.tsx
@@ -17,7 +17,7 @@ export default function ToolPage(props: Props) {
     const [tool, setTool] = useState<Tools>()
     const [isBeingEdited, setIsBeingEdited] = useState(false)
     const [loading, setLoading] = useState(false);
-
+    const [isModalOpen, setModalOpen] = useState(false);
     const [name, setName] = useState<string>("")
     const [selectedOptions, setSelectedOptions] = useState<{
         label: string;
@@ -66,9 +66,18 @@ export default function ToolPage(props: Props) {
                 props.onToolUpdate?.()
             })
             .catch(error => console.error(error))
-            .finally(() => setLoading(false));
+            .finally(() => setLoading(false))
+            .finally(() => setModalOpen(false));
     }
 
+    const handleDeleteClick = () => {
+        setModalOpen(true);
+    };
+
+    const handleCancelClick = () => {
+        setModalOpen(false);
+    };
+    console.log(isModalOpen)
     const isoTime = tool?.timestamp ?? new Date().toISOString()
     const formattedTimeStamp = new Date(isoTime).toLocaleString([], {
         day: '2-digit',
@@ -213,54 +222,51 @@ export default function ToolPage(props: Props) {
                 </form>
         }
         {buttonContent}
-        {
-            isLoggedIn
+        {isLoggedIn
+            ?
+            loading
                 ?
-                loading
-                    ?
-                    <svg className={"buttonLarge"}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 100 100"
-                        preserveAspectRatio="xMidYMid"
-                        style={{
-                            margin: 'auto',
-                            display: 'block',
-                            shapeRendering: 'auto'
-                        }}
-                        width="40px"
-                        height="40px"
-                    >
-                        <g transform="translate(50 50)">
-                            <g>
-                                <animateTransform attributeName="transform"
-                                                  type="rotate"
-                                                  values="0;45"
-                                                  keyTimes="0;1"
-                                                  dur="1000ms"
-                                                  repeatCount="indefinite">
-                                </animateTransform>
-                                <path
-                                    fill={"#ffffff" }
-                                    d="M29.491524206117255 -5.5 L37.491524206117255 -5.5 L37.491524206117255 5.5 L29.491524206117255 5.5 A30 30 0 0 1 24.742744050198738 16.964569457146712 L24.742744050198738 16.964569457146712 L30.399598299691117 22.621423706639092 L22.621423706639096 30.399598299691114 L16.964569457146716 24.742744050198734 A30 30 0 0 1 5.5 29.491524206117255 L5.5 29.491524206117255 L5.5 37.491524206117255 L-5.499999999999997 37.491524206117255 L-5.499999999999997 29.491524206117255 A30 30 0 0 1 -16.964569457146705 24.742744050198738 L-16.964569457146705 24.742744050198738 L-22.621423706639085 30.399598299691117 L-30.399598299691117 22.621423706639092 L-24.742744050198738 16.964569457146712 A30 30 0 0 1 -29.491524206117255 5.500000000000009 L-29.491524206117255 5.500000000000009 L-37.491524206117255 5.50000000000001 L-37.491524206117255 -5.500000000000001 L-29.491524206117255 -5.500000000000002 A30 30 0 0 1 -24.742744050198738 -16.964569457146705 L-24.742744050198738 -16.964569457146705 L-30.399598299691117 -22.621423706639085 L-22.621423706639092 -30.399598299691117 L-16.964569457146712 -24.742744050198738 A30 30 0 0 1 -5.500000000000011 -29.491524206117255 L-5.500000000000011 -29.491524206117255 L-5.500000000000012 -37.491524206117255 L5.499999999999998 -37.491524206117255 L5.5 -29.491524206117255 A30 30 0 0 1 16.964569457146702 -24.74274405019874 L16.964569457146702 -24.74274405019874 L22.62142370663908 -30.39959829969112 L30.399598299691117 -22.6214237066391 L24.742744050198738 -16.964569457146716 A30 30 0 0 1 29.491524206117255 -5.500000000000013 M0 -20A20 20 0 1 0 0 20 A20 20 0 1 0 0 -20"
-                                ></path>
-                            </g>
+                <svg className={"buttonLarge"}
+                     xmlns="http://www.w3.org/2000/svg"
+                     viewBox="0 0 100 100"
+                     preserveAspectRatio="xMidYMid"
+                     style={{
+                         margin: 'auto',
+                         display: 'block',
+                         shapeRendering: 'auto'
+                     }}
+                     width="40px"
+                     height="40px"
+                >
+                    <g transform="translate(50 50)">
+                        <g>
+                            <animateTransform attributeName="transform"
+                                              type="rotate"
+                                              values="0;45"
+                                              keyTimes="0;1"
+                                              dur="1000ms"
+                                              repeatCount="indefinite">
+                            </animateTransform>
+                            <path
+                                fill={"#ffffff"}
+                                d="M29.491524206117255 -5.5 L37.491524206117255 -5.5 L37.491524206117255 5.5 L29.491524206117255 5.5 A30 30 0 0 1 24.742744050198738 16.964569457146712 L24.742744050198738 16.964569457146712 L30.399598299691117 22.621423706639092 L22.621423706639096 30.399598299691114 L16.964569457146716 24.742744050198734 A30 30 0 0 1 5.5 29.491524206117255 L5.5 29.491524206117255 L5.5 37.491524206117255 L-5.499999999999997 37.491524206117255 L-5.499999999999997 29.491524206117255 A30 30 0 0 1 -16.964569457146705 24.742744050198738 L-16.964569457146705 24.742744050198738 L-22.621423706639085 30.399598299691117 L-30.399598299691117 22.621423706639092 L-24.742744050198738 16.964569457146712 A30 30 0 0 1 -29.491524206117255 5.500000000000009 L-29.491524206117255 5.500000000000009 L-37.491524206117255 5.50000000000001 L-37.491524206117255 -5.500000000000001 L-29.491524206117255 -5.500000000000002 A30 30 0 0 1 -24.742744050198738 -16.964569457146705 L-24.742744050198738 -16.964569457146705 L-30.399598299691117 -22.621423706639085 L-22.621423706639092 -30.399598299691117 L-16.964569457146712 -24.742744050198738 A30 30 0 0 1 -5.500000000000011 -29.491524206117255 L-5.500000000000011 -29.491524206117255 L-5.500000000000012 -37.491524206117255 L5.499999999999998 -37.491524206117255 L5.5 -29.491524206117255 A30 30 0 0 1 16.964569457146702 -24.74274405019874 L16.964569457146702 -24.74274405019874 L22.62142370663908 -30.39959829969112 L30.399598299691117 -22.6214237066391 L24.742744050198738 -16.964569457146716 A30 30 0 0 1 29.491524206117255 -5.500000000000013 M0 -20A20 20 0 1 0 0 20 A20 20 0 1 0 0 -20"
+                            ></path>
                         </g>
-                    </svg>
-                    : <ButtonLarge name={"Löschen"} onClick={() => tool?.id && deleteToolById(tool.id)}/>
-
-                : null
+                    </g>
+                </svg>
+                : <>
+                    <ButtonLarge name={"Lö"} onClick={handleDeleteClick}/>
+                    {isModalOpen &&
+                        <div className="deleteConfirmation">
+                            <p>Wollen Sie das Element wirklich löschen?</p>
+                            <div>
+                                <ButtonLarge name={"Abbrechen"} onClick={handleCancelClick}/>
+                                <ButtonLarge name={"Löschen"} onClick={() => tool?.id && deleteToolById(tool.id)}/>
+                            </div>
+                        </div>
+                    }
+                </>
+            : <></>
         }
-
-    </article>)
-        ;
+    </article>);
 }
-
-/*
-PostMapping
-@RequestPart MultipartFile "VariableName"
-
-Ggf. Service
-Byte[] byte= MultipartFile.getBytes();
-Base64.getEncoder().encodeToString(byte);
-
-Base64 importieren von Java.util*/
