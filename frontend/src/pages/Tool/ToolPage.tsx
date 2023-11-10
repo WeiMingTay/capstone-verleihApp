79bbaada8bbf817d981dedf86ad74e04c73b7684
weimingtay <ming23.tay@gmail.com>
2023-11-10 18:21:23 +0700

    LadeZahnrad auf DeleteButton
    TO-DO:
    - A11y auf hamburger menü

--- frontend/src/pages/Tool/ToolPage.tsx
+++ frontend/src/pages/Tool/ToolPage.tsx
@@ -16,6 +16,7 @@ type Props = {
 export default function ToolPage(props: Props) {
     const [tool, setTool] = useState<Tools>()
     const [isBeingEdited, setIsBeingEdited] = useState(false)
+    const [loading, setLoading] = useState(false);
 
     const [name, setName] = useState<string>("")
     const [selectedOptions, setSelectedOptions] = useState<{
@@ -58,12 +59,14 @@ export default function ToolPage(props: Props) {
     }
 
     function deleteToolById(id: string) {
+        setLoading(true);
         axios.delete(`/api/tools/${id}`)
             .then(() => {
                 navigate("/werkzeuge")
                 props.onToolUpdate?.()
             })
             .catch(error => console.error(error))
+            .finally(() => setLoading(false));
     }
 
     const isoTime = tool?.timestamp ?? new Date().toISOString()
@@ -100,10 +103,12 @@ export default function ToolPage(props: Props) {
         const newName: string = event.target.value;
         setName(newName);
     }
+
     const catOptions = allCategories.map(category => ({
         label: capitalizeWords(getCategoryTranslation(category)),
         value: category
     }));
+
     function changeLocation(event: ChangeEvent<HTMLInputElement>) {
         const newLocation: string = event.target.value;
         setLocation(newLocation)
@@ -118,18 +123,19 @@ export default function ToolPage(props: Props) {
         const newDescription: string = event.target.value;
         setDescription(newDescription)
     }
+
     let buttonContent;
 
     if (isLoggedIn) {
         if (!isBeingEdited) {
             buttonContent = (
-                <ButtonLarge name="Bearbeiten" onClick={() => setIsBeingEdited(true)} />
+                <ButtonLarge name="Bearbeiten" onClick={() => setIsBeingEdited(true)}/>
             );
         } else {
             buttonContent = (
                 <div className={"edit-btn"}>
-                    <ButtonLarge name={"Abbrechen"} onClick={() => setIsBeingEdited(false)} />
-                    <ButtonLarge name={"Speichern"} onClick={() => setIsBeingEdited(false)} />
+                    <ButtonLarge name={"Abbrechen"} onClick={() => setIsBeingEdited(false)}/>
+                    <ButtonLarge name={"Speichern"} onClick={() => setIsBeingEdited(false)}/>
                 </div>
             );
         }
@@ -188,9 +194,9 @@ export default function ToolPage(props: Props) {
                     </label>
 
                     <label id="location-input">Ort: <input type="text"
-                                       value={location ?? tool?.location}
-                                       placeholder="Ort"
-                                       onChange={changeLocation}/>
+                                                           value={location ?? tool?.location}
+                                                           placeholder="Ort"
+                                                           onChange={changeLocation}/>
                     </label>
                     <label id="author-input">Ansprechpartner:in <input
                         type="text"
@@ -199,9 +205,9 @@ export default function ToolPage(props: Props) {
                         onChange={changeAuthor}/>
                     </label>
                     <label id="description-input">Anleitung/ Beschreibung: <textarea rows={5}
-                        value={description ?? tool?.description}
+                                                                                     value={description ?? tool?.description}
 
-                        onChange={changeDescription}/>
+                                                                                     onChange={changeDescription}/>
                     </label>
                     <button style={{display: "none"}}>Save</button>
                 </form>
@@ -209,7 +215,39 @@ export default function ToolPage(props: Props) {
         {buttonContent}
         {
             isLoggedIn
-                ? <ButtonLarge name={"Löschen"} onClick={() => tool?.id && deleteToolById(tool.id)}/>
+                ?
+                loading
+                    ?
+                    <svg className={"buttonLarge"}
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 100 100"
+                        preserveAspectRatio="xMidYMid"
+                        style={{
+                            margin: 'auto',
+                            display: 'block',
+                            shapeRendering: 'auto'
+                        }}
+                        width="40px"
+                        height="40px"
+                    >
+                        <g transform="translate(50 50)">
+                            <g>
+                                <animateTransform attributeName="transform"
+                                                  type="rotate"
+                                                  values="0;45"
+                                                  keyTimes="0;1"
+                                                  dur="1000ms"
+                                                  repeatCount="indefinite">
+                                </animateTransform>
+                                <path
+                                    fill={"#ffffff" }
+                                    d="M29.491524206117255 -5.5 L37.491524206117255 -5.5 L37.491524206117255 5.5 L29.491524206117255 5.5 A30 30 0 0 1 24.742744050198738 16.964569457146712 L24.742744050198738 16.964569457146712 L30.399598299691117 22.621423706639092 L22.621423706639096 30.399598299691114 L16.964569457146716 24.742744050198734 A30 30 0 0 1 5.5 29.491524206117255 L5.5 29.491524206117255 L5.5 37.491524206117255 L-5.499999999999997 37.491524206117255 L-5.499999999999997 29.491524206117255 A30 30 0 0 1 -16.964569457146705 24.742744050198738 L-16.964569457146705 24.742744050198738 L-22.621423706639085 30.399598299691117 L-30.399598299691117 22.621423706639092 L-24.742744050198738 16.964569457146712 A30 30 0 0 1 -29.491524206117255 5.500000000000009 L-29.491524206117255 5.500000000000009 L-37.491524206117255 5.50000000000001 L-37.491524206117255 -5.500000000000001 L-29.491524206117255 -5.500000000000002 A30 30 0 0 1 -24.742744050198738 -16.964569457146705 L-24.742744050198738 -16.964569457146705 L-30.399598299691117 -22.621423706639085 L-22.621423706639092 -30.399598299691117 L-16.964569457146712 -24.742744050198738 A30 30 0 0 1 -5.500000000000011 -29.491524206117255 L-5.500000000000011 -29.491524206117255 L-5.500000000000012 -37.491524206117255 L5.499999999999998 -37.491524206117255 L5.5 -29.491524206117255 A30 30 0 0 1 16.964569457146702 -24.74274405019874 L16.964569457146702 -24.74274405019874 L22.62142370663908 -30.39959829969112 L30.399598299691117 -22.6214237066391 L24.742744050198738 -16.964569457146716 A30 30 0 0 1 29.491524206117255 -5.500000000000013 M0 -20A20 20 0 1 0 0 20 A20 20 0 1 0 0 -20"
+                                ></path>
+                            </g>
+                        </g>
+                    </svg>
+                    : <ButtonLarge name={"Löschen"} onClick={() => tool?.id && deleteToolById(tool.id)}/>
+
                 : null
         }
 
