0cf56f8468a795e63cd821eb760bf5e7aed33589
weimingtay <ming23.tay@gmail.com>
2023-11-22 23:52:35 +0800

    console.log function to post and delete button

--- frontend/src/pages/SchwarzesBrett/SchwarzesBrett.tsx
+++ frontend/src/pages/SchwarzesBrett/SchwarzesBrett.tsx
@@ -1,16 +1,25 @@
 import "./SchwarzesBrett.scss";
 
 export default function SchwarzesBrett() {
+
+    function addPost() {
+        console.log("addPost");
+
+    }
+    function deletePost() {
+        console.log("deletePost");
+    }
     return (
         <div className="schwarzesBrett-page">
             <section>
                 <div>
                     <h5>Ich suche</h5>
-                    <button className="btn btn-primary">+</button>
+                    <button onClick={addPost} className="btn btn-primary add-btn">+</button>
                 </div>
                 <div>
                     <article>
                         <p>Schlagzeug</p>
+                        <button onClick={deletePost} className="delete-btn">-</button>
                     </article>
                 </div>
             </section>
