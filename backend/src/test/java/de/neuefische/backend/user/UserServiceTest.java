b6ffdee9ca6b859fa68cb1f678348d088db4c32f
weimingtay <ming23.tay@gmail.com>
2023-11-26 20:21:43 +0800

    UserServiceTest TO-DO: Neue Tests für Oauth mit google und USer schreiben

--- backend/src/test/java/de/neuefische/backend/user/UserServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/user/UserServiceTest.java
@@ -1,52 +1,65 @@
 package de.neuefische.backend.user;
 
-import de.neuefische.backend.user.UserProfile;
-import de.neuefische.backend.user.UserRepository;
-import de.neuefische.backend.user.UserService;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
-import java.util.NoSuchElementException;
-import java.util.Optional;
-
-import static org.mockito.Mockito.when;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserServiceTest {
 
     @Mock
+    private OAuth2AuthenticationToken mockOAuth2AuthenticationToken;
+    @Mock
     private UserRepository mockUserRepository;
-
-    @InjectMocks
     private UserService userService;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
+
+        userService = new UserService(mockUserRepository);
     }
 
     @Test
-    void getUserProfileById_shouldReturnUserProfile() {
-        UserProfile mockUserProfile = new UserProfile("12345", "testuser", "https://example.com/avatar.jpg", "testuser@example.com");
+    void createUserProfile_shouldReturnUserProfile() {
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("id", "12345");
+        attributes.put("login", "testuser");
+        attributes.put("avatar_url", "https://example.com/avatar.jpg");
+        attributes.put("email", "testuser@example.com");
 
-        when(mockUserRepository.findById("12345")).thenReturn(Optional.of(mockUserProfile));
+        OAuth2User oauth2User = new DefaultOAuth2User(
+                Set.of(new SimpleGrantedAuthority("ROLE_USER")),
+                attributes,
+                "login"
+        );
 
-        UserProfile userProfile = userService.getUserProfileById("12345");
+        when(mockOAuth2AuthenticationToken.getPrincipal()).thenReturn(oauth2User);
+
+        UserProfile userProfile = userService.createUserProfile(mockOAuth2AuthenticationToken);
 
         assertEquals("12345", userProfile.id());
         assertEquals("testuser", userProfile.name());
         assertEquals("https://example.com/avatar.jpg", userProfile.avatarUrl());
         assertEquals("testuser@example.com", userProfile.email());
     }
-
     @Test
     void getUserProfileById_shouldThrowNoSuchElementException() {
+        // Mocking the behavior of the UserRepository to return an empty optional
         when(mockUserRepository.findById("nonexistentUserId")).thenReturn(Optional.empty());
 
+        // Calling the method under test and expecting an exception
         assertThrows(NoSuchElementException.class, () -> userService.getUserProfileById("nonexistentUserId"));
     }
 }
